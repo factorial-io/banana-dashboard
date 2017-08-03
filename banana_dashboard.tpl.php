59b3e260793c946464689195ea0185e1fe36da71
D34dMan <shibin@factorial.io>
2017-08-04 02:15:24 +0530

    Introduce Categories for Dashboard menu items

--- banana_dashboard.tpl.php
+++ banana_dashboard.tpl.php
@@ -7,13 +7,15 @@
 ?>
 <div id="banana_dashboard">
   <?php foreach ($dashboard_menu as $group => $menu_items): ?>
-    <ul class="banana_dashboard_menu menu inline group-<?php echo $group; ?>">
-      <span class="label"><?php echo $group ?></span>
-      <?php foreach($menu_items as $menu_item): ?>
-        <li class="dashboard-menu-item" >
-          <a href="<?php echo $menu_item['url']; ?>" class="banana-dashboard-menu-link  <?php echo 'dashboard-icon-' . $menu_item['icon'] ?>"><?php echo $menu_item['title'] ?></a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
+    <div class="banana-dashboard-group banana-dashboard-group-group-<?php echo $group; ?>">
+      <h4 class="label"><?php echo $group ?></h4>
+      <ul class="banana_dashboard_menu menu inline">
+        <?php foreach($menu_items as $menu_item): ?>
+          <li class="dashboard-menu-item" >
+            <a href="<?php echo $menu_item['url']; ?>" class="banana-dashboard-menu-link  <?php echo 'dashboard-icon-' . $menu_item['icon'] ?>"><?php echo $menu_item['title'] ?></a>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    </div>
   <?php endforeach ?>
 </div>
