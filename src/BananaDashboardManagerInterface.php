8d5c1e773e309a7862ebaf2f27496649bbc1034f
Ashmika Hirani <ashmika@factorial.io>
2019-09-10 01:25:41 -0400

    Added a new interface for banana dashboard manager

---
+++ src/BananaDashboardManagerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\banana_dashboard;
+
+use Drupal\Component\Plugin\PluginManagerInterface;
+
+/**
+ * Defines an interface for banana_dashboard managers.
+ */
+interface BananaDashboardManagerInterface extends PluginManagerInterface {
+
+    // Add getters and other public methods for banana_dashboard managers.
+
+}
